c05c8ac527ab026d70705ec741df83f811d792da
raphael-attias <raphael.attias@laplateforme.io>
2023-12-14 21:21:25 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import socket
+from ip2geotools.databases.noncommercial import DbIpCity
+from geopy.geocoders import Nominatim
+
+url = input("Insert a URL: ")
+ip = socket.gethostbyname(url)
+response = DbIpCity.get(ip, api_key="free")
+
+geolocator = Nominatim(user_agent="geo_locator")
+location = geolocator.geocode(response.city)
+
+print(f"IP: {ip}")
+print(f"Ville: {response.city}")
+print(f"Région: {response.region}")
+print(f"Pays: {response.country}")
+
+if location:
+    print(f"Adresse complète: {location.address}")
+    print(f"Latitude: {location.latitude}")
+    print(f"Longitude: {location.longitude}")
+else:
+    print("Impossible de récupérer les coordonnées géographiques.")
